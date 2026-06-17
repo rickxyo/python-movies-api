c81144e280cd3a050b929568ee1b086dc2366ad2
rickxyo <pedrinhooldoni@gmail.com>
2026-06-17 18:04:48 -0300

    feat(genres): add list endpoint for genres

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import JsonResponse
+from genres.views import genre_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('genres/', genre_view, name='genre-list')
 ]
