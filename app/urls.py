ec99714e24ce83ee88c805285ff7d23f92ccc62a
rickxyo <pedrinhooldoni@gmail.com>
2026-06-23 00:04:05 -0300

    feat(genres) add post endpoint for genres

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,5 @@ from genres.views import genre_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', genre_view, name='genre-list')
+    path('genres/', genre_view, name='genre')
 ]
