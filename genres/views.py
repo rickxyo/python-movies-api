8afbbc5d98d823713094b160161b04c08866ff82
rickxyo <pedrinhooldoni@gmail.com>
2026-06-23 01:08:58 -0300

    feat(genres): add detail endpoint for genres

--- genres/views.py
+++ genres/views.py
@@ -1,6 +1,7 @@
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 from genres.models import Genre
 
 @csrf_exempt
@@ -15,4 +16,10 @@ def genre_create_list_view(request):
         new_genre.save()
         return JsonResponse({'id': new_genre.id, 'name': new_genre.name}, status=201)
         
+@csrf_exempt
+def genre_detail_view(request, pk):
+    genre = get_object_or_404(Genre, pk=pk)
+    data = {'id': genre.id, 'name': genre.name}
+    return JsonResponse(data)
+    
 
