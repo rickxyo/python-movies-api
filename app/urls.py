f6d239bacc06b2c01cb65ce01cbd1154f45abef1
rickxyo <pedrinhooldoni@gmail.com>
2026-07-01 11:57:49 -0300

    refactor(genres): replace detail view with DRF generic view

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from genres.views import GenreCreateListVieww, genre_detail_view
+from genres.views import GenreCreateListVieww, GenreRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('genres/', GenreCreateListVieww.as_view(), name='genre-create-list'),
-    path('genres/<int:pk>/', genre_detail_view, name='genre-detail-view'),
+    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
 ]
