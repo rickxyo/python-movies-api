b73ce1ecf9e5076a849ff826e98244fe086763b7
rickxyo <pedrinhooldoni@gmail.com>
2026-07-06 11:46:12 -0300

    feat(actors): register actor list and detail endpoints

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from genres.views import GenreCreateListVieww, GenreRetrieveUpdateDestroyView
+from actors.views import ActorCreateListView, ActorRetriveUpdateDestroyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('genres/', GenreCreateListVieww.as_view(), name='genre-create-list'),
     path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
+    path('actors/', ActorCreateListView.as_view(), name='actor-create-list'),
+    path('actors/<int:pk>/', ActorRetriveUpdateDestroyView.as_view(), name='actor-detail-list')
+    
+    
 ]
