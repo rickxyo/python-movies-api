52710f39c0c0c773b1a8bee8009b846846918d3c
rickxyo <pedrinhooldoni@gmail.com>
2026-07-21 16:45:33 -0300

    feat(movies): validate release date and resume

--- movies/serializers.py
+++ movies/serializers.py
@@ -5,3 +5,13 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+        
+    def validate_release_date(self, value):
+        if value.year < 1895:
+            raise serializers.ValidationError('Release date cannot be earlier than 1895.')
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError('Description cannot exceed 200 characters')
+        return value
