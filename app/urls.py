1e992f2bb00943ae48cbf870b7bd7548a2295cc3
rickxyo <pedrinhooldoni@gmail.com>
2026-07-21 11:13:21 -0300

    refactor(urls): implement modular routing with api versioning

--- app/urls.py
+++ app/urls.py
@@ -1,22 +1,11 @@
 from django.contrib import admin
-from django.urls import path
-from genres.views import GenreCreateListVieww, GenreRetrieveUpdateDestroyView
-from actors.views import ActorCreateListView, ActorRetriveUpdateDestroyView
-from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
-from reviews.views import ReviewCreateListView, ReviewRetrieveUpdateDestroyView
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path('genres/', GenreCreateListVieww.as_view(), name='genre-create-list'),
-    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
-    
-    path('actors/', ActorCreateListView.as_view(), name='actor-create-list'),
-    path('actors/<int:pk>/', ActorRetriveUpdateDestroyView.as_view(), name='actor-detail-view'),
-    
-    path('movies/', MovieCreateListView.as_view(), name='movie-create-list'),
-    path('movies/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view'),
-    
-    path('review/', ReviewCreateListView.as_view(), name='review-create-list'),
-    path('review/<int:pk>/', ReviewRetrieveUpdateDestroyView.as_view(), name='review-detail-view')
+    path('api/v1/', include('genres.urls')),
+    path('api/v1/', include('actors.urls')),
+    path('api/v1/', include('movies.urls')),
+    path('api/v1/', include('reviews.urls')),
 ]
