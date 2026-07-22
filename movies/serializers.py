987047e264a58e828d6e3a346fb68320510ec6f5
rickxyo <pedrinhooldoni@gmail.com>
2026-07-22 11:57:59 -0300

    feat(movies): add get_rate function

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,17 +1,28 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from movies.models import Movie
 
 class MovieSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
+    
     class Meta:
         model = Movie
         fields = '__all__'
         
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+        
+        if rate:
+            return rate
+        
+        return None
+    
     def validate_release_date(self, value):
         if value.year < 1895:
             raise serializers.ValidationError('Release date cannot be earlier than 1895.')
         return value
     
     def validate_resume(self, value):
-        if len(value) > 200:
+        if len(value) > 500:
             raise serializers.ValidationError('Description cannot exceed 200 characters')
         return value
