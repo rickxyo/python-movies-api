481db7802952bfbc9e42ebf053858dca5fda25e0
rickxyo <pedrinhooldoni@gmail.com>
2026-07-23 11:39:17 -0300

    feat(auth): add authentication app and routes

--- app/settings.py
+++ app/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
+    'authentication',
     'genres',
     'actors',
     'movies',
@@ -125,3 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
