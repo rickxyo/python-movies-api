9b0c722c4e645aee2b86e40da0e4e29b2b2ed0c1
rickxyo <pedrinhooldoni@gmail.com>
2026-07-29 12:10:38 -0300

    fix(genres): add PATCH/PUT/DELETE permission checks

--- genres/permissions.py
+++ genres/permissions.py
@@ -8,4 +8,8 @@ class GenrePermissionClass(permissions.BasePermission):
             return request.user.has_perm('genres.view_genre')
         if request.method == 'POST':
             return request.user.has_perm('genres.add_genre')
+        if request.method in ['PATCH', 'PUT']:
+            return request.user.has_perm('genres.change_genre')
+        if request.method == 'DELETE':
+            return request.user.has_perm('genres.delete_genre')
         return False
