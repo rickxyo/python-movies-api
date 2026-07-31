6ba089123c248ca77caf17f5944b4326e08c47a5
rickxyo <pedrinhooldoni@gmail.com>
2026-07-31 16:47:05 -0300

    feat(movies): add aggregate movie statistics to stats view

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,10 @@
+from django.db.models import Count, Avg
 from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
 from app.permissions import GlobalDefaultPermission
 from movies.models import Movie
 from movies.serializers import MovieSerializer
+from reviews.models import Review
 
 class MovieCreateListView(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
@@ -19,7 +21,14 @@ class MovieStatsView(views.APIView):
     queryset = Movie.objects.all()
     
     def get(self, request):
-        return response.Response(
-            data={'message': 'OK'},
-            status=status.HTTP_200_OK
-        )
+        total_movies = self.queryset.count()
+        movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id'))
+        total_reviews = Review.objects.count()
+        average_stars = Review.objects.aggregate(avg_stars=Avg('stars'))['avg_stars']
+        
+        return response.Response(data={
+            'total_movies': total_movies,
+            'movies_by_genre': movies_by_genre,
+            'total_reviews': total_reviews,
+            'average_stars': round(average_stars, 1) if average_stars else 0,
+        }, status=status.HTTP_200_OK)
