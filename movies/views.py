33a91d1faf3e60cf20fef75d68cd79b92ed1c940
rickxyo <pedrinhooldoni@gmail.com>
2026-07-31 12:07:49 -0300

    feat(movies): add new movies URL and view

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
 from app.permissions import GlobalDefaultPermission
 from movies.models import Movie
@@ -13,3 +13,13 @@ class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+class MovieStatsView(views.APIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+    queryset = Movie.objects.all()
+    
+    def get(self, request):
+        return response.Response(
+            data={'message': 'OK'},
+            status=status.HTTP_200_OK
+        )
